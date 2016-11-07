ae6e88bcbf4a32d5b463119cf22d99f1f4e7fa25
Artem Kondratyev <artemkv@gmail.com>
2016-11-07 18:42:46 +0100

    Search UI (w/o logic)

--- NotedOK/app/src/main/java/com/notedok/notedok/NoteListViewActivity.java
+++ NotedOK/app/src/main/java/com/notedok/notedok/NoteListViewActivity.java
@@ -1,13 +1,17 @@
 package com.notedok.notedok;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -76,10 +80,15 @@ public class NoteListViewActivity extends AppCompatActivity implements MasterAct
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // TODO: do we need this?
-
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_note_list, menu);
+
+        // Setup search
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
 
